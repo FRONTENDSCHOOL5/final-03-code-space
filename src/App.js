68c7f28ac3c0bd00be1f88068fc37816e8e8ec1b
ChoByungHyun <cbj973@gmail.com>
2023-06-08 19:55:02 +0900

    💄 Styles : globalStyle, normalize.css  SetUp.
    Co-authored-by: 백서영 <seoye0ng@users.noreply.github.com>
    Co-authored-by: Song Yunbi <yunbiyomi@naver.com>
    Co-authored-by: garam <garamse@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import GlobalStyle from './globalStyle';
+import BottomNav from './Components/Common/BottomNav';
+import MainHeader from './Components/Common/MainHeader';
+import LoginHeader from './Components/Common/LoginHeader';
+import MainPage from './Pages/MainPage';
 function App() {
   return (
     <div>
-      hello world
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/btmnav" element={<BottomNav />} />
+        <Route path="/loginheader" element={<LoginHeader />} />
+        <Route path="/mainheader" element={<MainHeader />} />
+      </Routes>
     </div>
   );
 }
