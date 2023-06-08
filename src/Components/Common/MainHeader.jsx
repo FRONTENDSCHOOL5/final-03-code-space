239d8bedd72df246f13ebde79debd897e1aa03a5
ChoByungHyun <cbj973@gmail.com>
2023-06-08 19:59:10 +0900

    ✨ Feat : MainHeader 컴포넌트 구현.
    Co-authored-by: 백서영 <seoye0ng@users.noreply.github.com>
    Co-authored-by: Song Yunbi <yunbiyomi@naver.com>
    Co-authored-by: garam <garamse@users.noreply.github.com>

---
+++ src/Components/Common/MainHeader.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+import backIcon from '../../assets/icon-arrow-left.svg';
+import searchIcon from '../../assets/icon-search.svg';
+import Button from './Button';
+const MainHeader = ({ type, leftChild, rightChild }) => {
+  leftChild = '코드허브 피드';
+  type = 'feed';
+  return (
+    <>
+      <SLayout>
+        {type === 'feed' ? (
+          <>
+            <div>{leftChild}</div>
+            <img src={searchIcon} alt="돋보기"></img>
+          </>
+        ) : (
+          <>
+            <img src={backIcon} alt="뒤로가기"></img>
+            <SSearch type="text" placeholder="Search" />
+          </>
+        )}
+      </SLayout>
+    </>
+  );
+};
+
+export default MainHeader;
+const SLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 390px;
+  max-height: 48px;
+  margin: 0 auto;
+  border-bottom: 1px solid #767676;
+  background-color: #1e1e1e;
+  color: #f8f8f8;
+  padding: 15px;
+  box-sizing: border-box;
+  font-family: var(--title-font);
+  font-weight: bold;
+  align-items: center;
+`;
+const SSearch = styled.input`
+  display: flex;
+  flex: 1;
+  border-radius: 32px;
+  font-size: 14px;
+  padding: 6px 12px;
+  margin-left: 10px;
+  border: none;
+  box-sizing: border-box;
+`;
