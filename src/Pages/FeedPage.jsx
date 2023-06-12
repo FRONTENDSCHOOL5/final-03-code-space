e0c4ce1e863efd3254f08ea542937a347f3baa77
ChoByungHyun <cbj973@gmail.com>
2023-06-12 17:00:38 +0900

    ♻️ Refactor : feed api fetch -> axios로 변경

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -3,45 +3,69 @@ import styled from 'styled-components';
 import MainHeader from '../Components/Common/MainHeader';
 import BottomNav from '../Components/Common/BottomNav';
 import profileImg from '../assets/default-profile-image.svg';
+import axios from 'axios';
 
 const MainFeed = () => {
-  const BaseURL = 'https://api.mandarin.weniv.co.kr/';
+  const URL = 'https://api.mandarin.weniv.co.kr/';
   const FollowingPOST = 'post/feed';
   const Authorization =
     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NzZkNzZjYjJjYjIwNTY2MzJjZmZlYiIsImV4cCI6MTY5MDY5NDM4MCwiaWF0IjoxNjg1NTEwMzgwfQ.Bjwk8EyTTxyFP8-QYiY1SlXsAXTAYQ_Fwmi-nJ-NDx4';
 
   const APIDefaultImage = 'http://146.56.183.55:5050/Ellipse.png';
-  useEffect(() => {
-    fetchFeed();
-  }, []);
 
   const [fetchData, setfetchData] = useState(false);
   const [FeedList, setFeedList] = useState([]);
 
-  async function fetchFeed() {
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  const instance = axios.create({
+    baseURL: URL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: Authorization,
+    },
+  });
+
+  async function getFeed() {
     try {
-      const response = await fetch(`${BaseURL + FollowingPOST}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json;',
-          Authorization: Authorization,
-        },
-      });
-      if (!response.ok) {
-        throw new Error('네트워크에 문제가 있습니다.');
-      } else {
-        const data = await response.json();
-        const FeedListArr = [];
-        for (let i = 0; i < data.posts.length; i++) {
-          FeedListArr.push(data.posts[i]);
-        }
-        setFeedList(FeedListArr);
-        setfetchData(true);
+      const response = await instance.get(FollowingPOST);
+      const FeedListArr = [];
+      for (let i = 0; i < response.data.posts.length; i++) {
+        FeedListArr.push(response.data.posts[i]);
       }
+      setFeedList(FeedListArr);
+      setfetchData(true);
     } catch (error) {
-      console.error('데이터를 가져오는데 문제가 생겼습니다.', error);
+      console.error(error);
     }
   }
+
+  // async function fetchFeed() {
+  //   try {
+  //     const response = await fetch(`${BaseURL + FollowingPOST}`, {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json;',
+  //         Authorization: Authorization,
+  //       },
+  //     });
+  //     if (!response.ok) {
+  //       throw new Error('네트워크에 문제가 있습니다.');
+  //     } else {
+  //       const data = await response.json();
+  //       const FeedListArr = [];
+  //       for (let i = 0; i < data.posts.length; i++) {
+  //         FeedListArr.push(data.posts[i]);
+  //       }
+  //       setFeedList(FeedListArr);
+  //       setfetchData(true);
+  //     }
+  //   } catch (error) {
+  //     console.error('데이터를 가져오는데 문제가 생겼습니다.', error);
+  //   }
+  // }
 
   return (
     <>
