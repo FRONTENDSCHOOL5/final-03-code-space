0dff539da441d921e06d272a63ca32e2a89cb02b
seoye0ng <bsy0313@gmail.com>
2023-06-14 02:10:32 +0900

    ✨Feat : 판매중인 상품카드 컴포넌트 구현

---
+++ src/Components/Product/ProductCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+export default function ProductCard() {
+  return (
+    <SProductCard>
+      <SImage src="http://placeimg.com/640/480/100" alt="" />
+      <STitle>타이틀</STitle>
+      <SPrice>{}원</SPrice>
+    </SProductCard>
+  );
+}
+
+const SProductCard = styled.div`
+  box-shadow: inset 0px 0px 10px red;
+  width: 140px;
+`;
+
+const SImage = styled.img`
+  width: inherit;
+  height: 90px;
+  object-fit: cover;
+  border-radius: 8px;
+`;
+
+const STitle = styled.p`
+  margin: 6px 0 4px;
+  font-size: 14px;
+  color: var(--white);
+`;
+
+const SPrice = styled.p`
+  font-size: 12px;
+  font-weight: 700;
+  color: var(--point-color);
+`;
--- src/Pages/MyProfilePage.jsx
+++ src/Pages/MyProfilePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ProductCard from '../Components/Product/ProductList';
+
 const MyProfile = () => {
   return (
     <>
       <Link to="/follow">팔로우</Link>
       <Link to="/following">팔로잉</Link>
+
+      <ProductCard />
     </>
   );
 };
