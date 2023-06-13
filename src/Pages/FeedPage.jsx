34b9cbcec2061227525e8d5effe22b0d43db99f3
ChoByungHyun <cbj973@gmail.com>
2023-06-13 22:40:20 +0900

    ✨ Feat : 제목, 본문 나누는 함수 구현, 메인 피드 좋아요 댓글 구현

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -4,7 +4,10 @@ import MainHeader from '../Components/Common/MainHeader';
 import BottomNav from '../Components/Common/BottomNav';
 import profileImg from '../assets/default-profile-image.svg';
 import axios from 'axios';
+import iconHeart from '../assets/icons/heart.svg';
+import iconComment from '../assets/icons/chat-green.svg';
 
+// 피드 fetching, 게시글생성
 const MainFeed = () => {
   const URL = 'https://api.mandarin.weniv.co.kr/';
   const FollowingPOST = 'post/feed';
@@ -50,6 +53,12 @@ const MainFeed = () => {
       ) : (
         <div>
           {FeedList.map(item => {
+            const extractedData = extractString(item.content, 'title');
+            console.log(extractedData);
+            if (extractedData === null) {
+              return null; // 추출된 값이 없는 경우 해당 항목을 건너뜁니다.
+            }
+            const { extracted, remaining } = extractedData;
             return (
               <SFeedCard key={item.id}>
                 <SAuthor>
@@ -59,7 +68,7 @@ const MainFeed = () => {
                     <SProfileImg src={item.author.image} alt="프사" />
                   )}
                   <STitleContainer>
-                    <SContent>{item.content}</SContent>
+                    <STitle>{extracted}</STitle>
                     <SAuthorInfo>
                       <SUserName>{item.author.username}</SUserName>
                       <SAccountname>@{item.author.accountname}</SAccountname>
@@ -67,7 +76,19 @@ const MainFeed = () => {
                     </SAuthorInfo>
                   </STitleContainer>
                 </SAuthor>
-                <div>{/* <SContent>{item.content}</SContent> */}</div>
+                <div>
+                  <SContent>{remaining}</SContent>
+                </div>
+                <SReactionContainer>
+                  <SReactionCount>
+                    <SHeartImg src={iconHeart} alt="하트" />
+                    {item.heartCount}
+                  </SReactionCount>
+                  <SReactionCount>
+                    <SHeartImg src={iconComment} alt="댓글" />
+                    {item.comments.length}
+                  </SReactionCount>
+                </SReactionContainer>
               </SFeedCard>
             );
           })}
@@ -77,6 +98,27 @@ const MainFeed = () => {
   );
 };
 
+// 제목, 본문 찾기 함수
+function extractString(inputString, extractType) {
+  const startIndex = inputString.indexOf(`\\"${extractType}:`);
+  if (startIndex === -1) {
+    return null; // 해당 타입의 문자열이 없으면 null 반환
+  }
+
+  const endIndex = inputString.indexOf('\\"', startIndex + extractType.length + 3);
+  if (endIndex === -1) {
+    return null; // \\" 다음에 \\" 문자열이 없으면 null 반환
+  }
+
+  const extractedString = inputString.substring(startIndex + extractType.length + 3, endIndex);
+  const remainingString = inputString.substring(endIndex + 2);
+  return {
+    extracted: extractedString,
+    remaining: remainingString,
+  };
+}
+
+// 태그버튼 컴포넌트
 const TagButton = ({ text, active, onClick }) => {
   console.log(active);
   return (
@@ -86,6 +128,39 @@ const TagButton = ({ text, active, onClick }) => {
   );
 };
 
+// 피드 메인 페이지
+const FeedPage = () => {
+  const [toggle, setToggle] = useState('feed');
+  const [activeIndex, setActiveIndex] = useState(0);
+  const handleClick = index => {
+    setActiveIndex(index);
+  };
+
+  const tagItem = ['#전체', '#스터디 모집', '#질문있어요!', '#자유게시판'];
+  return (
+    <>
+      <SMain>
+        <MainHeader toggle={toggle} setToggle={setToggle} />
+        {toggle === 'feed' ? (
+          <>
+            <STagLayout>
+              {tagItem.map((item, index) => (
+                <TagButton text={item} active={index === activeIndex} onClick={() => handleClick(index)} />
+              ))}
+            </STagLayout>
+            <MainFeed />
+          </>
+        ) : (
+          <div>검색창</div>
+        )}
+        <BottomNav />
+      </SMain>
+    </>
+  );
+};
+export default FeedPage;
+
+// TagButton =====================================================
 const STagButton = styled.div`
   border-radius: 44px;
   border: 1px solid var(--darkgray);
@@ -98,41 +173,7 @@ const STagButton = styled.div`
     background-color: var(--point-color);
   }
 `;
-
-const FeedPage = () => {
-  const [toggle, setToggle] = useState('feed');
-  const [activeIndex, setActiveIndex] = useState(0);
-  const handleClick = index => {
-    setActiveIndex(index);
-    console.log(index);
-    console.log(activeIndex);
-  };
-
-  const tagItem = ['#전체', '#스터디 모집', '#질문있어요!', '#자유게시판'];
-  return (
-    <>
-      <SMain>
-        <MainHeader toggle={toggle} setToggle={setToggle} />
-        <STagLayout>
-          {tagItem.map((item, index) => (
-            <TagButton text={item} active={index === activeIndex} onClick={() => handleClick(index)} />
-          ))}
-        </STagLayout>
-
-        {toggle === 'feed' ? <MainFeed /> : <div>검색창</div>}
-        <BottomNav />
-      </SMain>
-    </>
-  );
-};
-export default FeedPage;
-
-const STagLayout = styled.div`
-  display: flex;
-  gap: 10px;
-  padding: 6px;
-`;
-
+// FeedPage =====================================================
 const SMain = styled.div`
   max-width: 390px;
   margin: 0 auto;
@@ -142,9 +183,24 @@ const SMain = styled.div`
   color: var(--white);
   font-family: var(--content-font);
 `;
+const STagLayout = styled.div`
+  display: flex;
+  gap: 10px;
+  padding: 6px;
+`;
 
+// MainFeed =====================================================
+
+const STitle = styled.div`
+  color: var(--white);
+  word-break: break-all;
+  font-weight: bold;
+`;
 const SContent = styled.div`
   color: var(--white);
+  font-size: 14px;
+  padding: 10px 5px;
+  box-sizing: border-box;
   word-break: break-all;
 `;
 const SUserName = styled.div`
@@ -188,4 +244,24 @@ const SAuthorInfo = styled.div`
   align-items: center;
   margin-top: 5px;
   gap: 5px;
+`;
+const SHeartImg = styled.img`
+  max-width: 15px;
+  max-height: 15px;
+  width: 100%;
+  height: 100%;
+  fill: var(--point-color);
+`;
+const SReactionContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  font-size: 12px;
+  color: var(--lightgray);
+  padding: 5px;
+`;
+const SReactionCount = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
 `;
