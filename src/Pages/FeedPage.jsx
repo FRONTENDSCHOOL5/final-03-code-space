6b589f9fe686f4791d29bccc4c3718f45a6b6201
ChoByungHyun <cbj973@gmail.com>
2023-06-13 21:22:24 +0900

    ✨ Feat : 태그 기능 구현

--- src/Pages/FeedPage.jsx
+++ src/Pages/FeedPage.jsx
@@ -61,7 +61,7 @@ const MainFeed = () => {
                   <STitleContainer>
                     <SContent>{item.content}</SContent>
                     <SAuthorInfo>
-                      <SContent>{item.author.username}</SContent>
+                      <SUserName>{item.author.username}</SUserName>
                       <SAccountname>@{item.author.accountname}</SAccountname>
                       <SAccountname>{item.createdAt.slice(0, 10)}</SAccountname>
                     </SAuthorInfo>
@@ -77,41 +77,90 @@ const MainFeed = () => {
   );
 };
 
-function goPostDetail() {}
+const TagButton = ({ text, active, onClick }) => {
+  console.log(active);
+  return (
+    <STagButton onClick={onClick} className={active ? 'active' : ''}>
+      {text}
+    </STagButton>
+  );
+};
+
+const STagButton = styled.div`
+  border-radius: 44px;
+  border: 1px solid var(--darkgray);
+  padding: 5px 8px;
+  font-size: 12px;
+  color: var(--lightgray);
+  cursor: pointer;
+  &.active {
+    color: var(--white);
+    background-color: var(--point-color);
+  }
+`;
 
 const FeedPage = () => {
   const [toggle, setToggle] = useState('feed');
+  const [activeIndex, setActiveIndex] = useState(0);
+  const handleClick = index => {
+    setActiveIndex(index);
+    console.log(index);
+    console.log(activeIndex);
+  };
+
+  const tagItem = ['#전체', '#스터디 모집', '#질문있어요!', '#자유게시판'];
   return (
-    <SMain>
-      <MainHeader toggle={toggle} setToggle={setToggle} />
-      {toggle === 'feed' ? <MainFeed /> : <div>검색창</div>}
-      <BottomNav />
-    </SMain>
+    <>
+      <SMain>
+        <MainHeader toggle={toggle} setToggle={setToggle} />
+        <STagLayout>
+          {tagItem.map((item, index) => (
+            <TagButton text={item} active={index === activeIndex} onClick={() => handleClick(index)} />
+          ))}
+        </STagLayout>
+
+        {toggle === 'feed' ? <MainFeed /> : <div>검색창</div>}
+        <BottomNav />
+      </SMain>
+    </>
   );
 };
 export default FeedPage;
+
+const STagLayout = styled.div`
+  display: flex;
+  gap: 10px;
+  padding: 6px;
+`;
 
 const SMain = styled.div`
   max-width: 390px;
   margin: 0 auto;
   background-color: var(--black);
-  height: 100vh;
+  min-height: 100vh;
+  padding-bottom: 64px;
   color: var(--white);
+  font-family: var(--content-font);
 `;
 
 const SContent = styled.div`
   color: var(--white);
   word-break: break-all;
 `;
+const SUserName = styled.div`
+  color: var(--lightgray);
+  word-break: break-all;
+  font-size: 13px;
+`;
 const SAccountname = styled.div`
   color: var(--darkgray);
-  font-size: 12px;
+  font-size: 10px;
 `;
 
 const SFeedCard = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid var(--point-color);
+  border: 1px solid var(--darkgray);
   padding: 20px;
   box-sizing: border-box;
 `;
