c2856fc2aaae3cb4de8c1b86d6a7c699e0b1b1fd
kimgaram <bd64118@naver.com>
2023-06-14 23:38:31 +0900

    🚚 Rename : LoginHeader->ProfileHeader 컴포넌트로 변경

---
+++ src/Components/Common/ProfileHeader.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function ProfileHeader({ HeadTitle, HeadTxt }) {
+  return (
+    <>
+      <SHeader>
+        <strong className="HeadTitle">{HeadTitle}</strong>
+        {HeadTxt && <p className="HeadTxt">{HeadTxt} </p>}
+      </SHeader>
+    </>
+  );
+}
+
+const SHeader = styled.header`
+  background-color: var(--black);
+  max-width: 390px;
+  margin: 0 auto;
+  padding-top: 40px;
+  text-align: center;
+
+  .HeadTitle {
+    color: #f8f8f8;
+    font-size: 24px;
+    font-family: var(--title-font);
+  }
+
+  .HeadTxt {
+    color: #cacaca;
+    font-size: 14px;
+    margin-top:-10px;
+    margin-bottom:50px;
+  }
+`;
