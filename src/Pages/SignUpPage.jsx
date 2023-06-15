092c85f5cc3789914383aeede76614eb345ea620
kimgaram <bd64118@naver.com>
2023-06-15 17:13:27 +0900

    ✨ Feat : 회원가입 기능 구현

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,8 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Modal from '../Components/Common/Modal';
 
 const SignUpPage = () => {
-  return <Link to="/profile">회원가입</Link>;
+  const navigate = useNavigate();
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [successRes, setSuccessRes] = useState('');
+
+  const inputHandler = e => {
+    if (e.target.type === 'email') {
+      setUserEmail(e.target.value);
+    } else if (e.target.type === 'password') {
+      setUserPassword(e.target.value);
+    }
+  };
+
+  const SignupSubmit = async e => {
+    e.preventDefault();
+
+    if (userPassword.length < 6) {
+      setIsPasswordValid(false);
+    } else {
+      setIsPasswordValid(true);
+    }
+
+    const url = 'https://api.mandarin.weniv.co.kr';
+
+    try {
+      const response = await axios.post(
+        url + '/user/emailvalid/',
+        {
+          user: {
+            email: userEmail,
+          },
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      setSuccessRes(response.data.message);
+      console.log(response.data.message)
+
+      if (successRes === '이미 가입된 이메일 주소 입니다.') {
+      } else if (successRes === '사용 가능한 이메일 입니다.') {
+        navigate('/profile');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Modal
+      title="이메일로 회원가입"
+      SignupSubmit={SignupSubmit}
+      userEmail={userEmail}
+      userPassword={userPassword}
+      inputHandler={inputHandler}
+      isPasswordValid={isPasswordValid}
+      successRes={successRes}
+    />
+  );
 };
 
 export default SignUpPage;
