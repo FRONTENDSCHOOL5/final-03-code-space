0f881967893eb180214d467f297e936a8a6aeb47
seoye0ng <bsy0313@gmail.com>
2023-06-16 22:43:24 +0900

    ✨Feat : 팔로우, 언팔로우 api 연동 (#13, #16)

--- src/Components/Profile/MainProfileBtns.jsx
+++ src/Components/Profile/MainProfileBtns.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import iconChat from '../../assets/icons/chat.png';
 import iconShare from '../../assets/icons/share.png';
 import Button from '../Common/Button';
+import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
+import { setToken } from '../../Atom/atom';
+import { setIsFollowed } from '../../Atom/atom';
+import axios from 'axios';
 
-export default function MainProfileBtns({ isMyProfile }) {
+export default function MainProfileBtns({ accountName, isMyProfile, setFollowerCount, followerCount }) {
+  const [isSubscribed, setIsSubscribed] = useRecoilState(setIsFollowed);
+  //   const [followerCount, setFollowerCount] = useState(2890);
+  const token = useRecoilValue(setToken);
+  const URL = 'https://api.mandarin.weniv.co.kr';
+
+  console.log(followerCount);
+
+  async function handleClick() {
+    if (isSubscribed === false) {
+      setIsSubscribed(true);
+      console.log(isSubscribed);
+      //   팔로워 추가해주기 -> api 요청
+      const reqPath = `/profile/${accountName}/follow`;
+      try {
+        const response = await axios(URL + reqPath, {
+          method: 'post',
+          headers: {
+            // 프로필 정보 요청 (토큰 필요)
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+          },
+        });
+        setFollowerCount(response.data.profile.followerCount);
+      } catch (error) {
+        console.log(error);
+      }
+    } else if (isSubscribed) {
+      setIsSubscribed(false);
+      //   팔로워 제거해주기 -> api 요청
+      const reqPath = `/profile/${accountName}/unfollow`;
+      try {
+        const response = await axios(URL + reqPath, {
+          method: 'DELETE',
+          headers: {
+            // 프로필 정보 요청 (토큰 필요)
+            Authorization: `Bearer ${token}`,
+            'Content-type': 'application/json',
+          },
+        });
+        setFollowerCount(response.data.profile.followerCount);
+
+        // console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   if (isMyProfile) {
     return (
       <SBtnLayout>
@@ -21,7 +73,9 @@ export default function MainProfileBtns({ isMyProfile }) {
     return (
       <SBtnContainer>
         <SChatBtn />
-        <Button width="120px">팔로우</Button>
+        <Button width="120px" onClick={handleClick} subscribed={isSubscribed}>
+          {isSubscribed ? '언팔로우 ' : '팔로우'}
+        </Button>
         <SShareBtn />
       </SBtnContainer>
     );
