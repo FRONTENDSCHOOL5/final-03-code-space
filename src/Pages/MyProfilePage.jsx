188ea3b377838e22eb70d9ffca9320d664fed730
seoye0ng <bsy0313@gmail.com>
2023-06-16 15:21:55 +0900

    ✨Feat : 상대방프로필 하단 네비바에서 내프로필을 클릭했을 때 내프로필 값을 전달 (#13, #14, #15, #16)

--- src/Pages/MyProfilePage.jsx
+++ src/Pages/MyProfilePage.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 import MainHeader from '../Components/Common/MainHeader';
 import BottomNav from '../Components/Common/BottomNav';
 import MainProfile from '../Components/Profile/MainProfile';
 import ProductList from '../Components/Product/ProductList';
+import MyProfileInfo from '../Components/Profile/MyProfileInfo';
 
 // 프로필을 클릭했을 때 useLocation으로 해당 profile 정보 가져오기
 function MyProfile() {
   const location = useLocation();
-  const profile = location.state;
-  console.log(profile);
+  let profile = location.state;
+  // console.log(profile);
+  // location으로 가져온 값이 있으면 그 값을, 값이 없다면 MyProfileInfo()값을 profile에 재할당
+  profile = profile ? profile : MyProfileInfo();
 
+  console.log(profile);
   return (
     <>
       <MainHeader />
       <MainProfile profile={profile} />
-      <ProductList accountName={profile.accountname} />
+      <ProductList profile={profile} />
       <BottomNav />
     </>
   );
