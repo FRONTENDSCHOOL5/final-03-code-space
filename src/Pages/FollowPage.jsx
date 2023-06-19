233f3ce472342626809e3f74ce1d571d2fd3debe
seoye0ng <bsy0313@gmail.com>
2023-06-19 17:07:02 +0900

    ✨ Feat : 팔로잉, 팔로워 페이지 api 불러오기 및 스타일 추가 (#13, #26)

--- src/Pages/FollowPage.jsx
+++ src/Pages/FollowPage.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { setToken } from '../Atom/atom';
+import axios from 'axios';
+import MainHeader from '../Components/Common/MainHeader';
+import BottomNav from '../Components/Common/BottomNav';
+import FollowListCard from '../Components/Follow/FollowListCard';
+import styled from 'styled-components';
 
-const FollowPage = () => {
-  return <div></div>;
-};
+export default function FollowPage() {
+  const token = useRecoilValue(setToken);
 
-export default FollowPage;
+  const location = useLocation();
+  let accountName = location.state.accountName;
+
+  console.log(accountName);
+
+  useEffect(() => {
+    // console.log('test');
+    getFollowData();
+  }, []);
+
+  const [followList, setFollowList] = useState([]);
+
+  const URL = 'https://api.mandarin.weniv.co.kr';
+  const reqPath = `/profile/${accountName}/follower`;
+
+  async function getFollowData() {
+    try {
+      const response = await axios.get(URL + reqPath, {
+        method: 'get',
+        headers: {
+          // 프로필 정보 요청 (토큰 필요)
+          Authorization: `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      });
+      console.log(response.data);
+
+      setFollowList(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // TODO: 메인계정은 안보이게 예외처리해주기!!
+
+  return (
+    <>
+      <MainHeader />
+      <SFollowerList>
+        {followList.map(follow => {
+          console.log(follow);
+          return <FollowListCard key={follow._id} profile={follow} isFollower={true} />;
+        })}
+      </SFollowerList>
+      <BottomNav />
+    </>
+  );
+}
+
+const SFollowerList = styled.div`
+  box-shadow: inset 0 0 30px red;
+  padding: 20px 16px;
+  color: var(--white);
+  font-family: var(--default-font);
+`;
