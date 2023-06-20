9a8411649fafd18d7424b3343adf226b6ae44a6d
seoye0ng <bsy0313@gmail.com>
2023-06-21 00:44:19 +0900

    💄 Styles : 프로필 페이지에서 쓰이는 버튼 컴포넌트 스타일 수정 및 추가 (#5, #13, #26)

--- src/Components/Common/Button.jsx
+++ src/Components/Common/Button.jsx
@@ -12,14 +12,14 @@ export default function Button(props) {
 
 const SButton = styled.button`
   width: ${props => props.width || '100%'};
-
+  font-family: var(--default-font);
   /* height: ${props => props.height || '100%'}; */
   background: ${props => props.bg || ''};
   background-color: ${props => props.bgColor || 'var(--point-color)'};
   color: ${props => props.color || 'var(--white)'};
   font-size: ${props => props.fontSize || '14px'};
-  font-weight: ${props => props.fontSize || '500'};
-  padding: 12px 0;
+  font-weight: ${props => props.fontWeight || '400'};
+  padding: ${props => props.padding || '12px 0'};
   border: ${props => props.border || 'none'};
   border-radius: ${props => props.borderRadius || '44px'};
   box-sizing: border-box;
@@ -32,16 +32,25 @@ const SButton = styled.button`
 
   ${props => props.subscribed && transparentStyle}
   ${props => props.myProfileBtn && transparentStyle}
+  ${props => props.followBtn && transparentStyle}
+  ${props => props.isFollowing && followStyle}
 `;
 
 const transparentStyle = css`
   background-color: transparent;
-  color: #cacaca;
+  color: var(--lightgray);
   box-shadow: 0px 0px 0px 1px var(--border-gray);
 
   &:hover {
     transition: all 0.2.5s ease-in-out;
-    color: var(--point-color);
-    background-color: var(--secondary-color);
+    color: var(--white);
+    background-color: var(--point-color);
+    box-shadow: none;
   }
+`;
+
+const followStyle = css`
+  background-color: var(--point-color);
+  color: var(--white);
+  box-shadow: none;
 `;
