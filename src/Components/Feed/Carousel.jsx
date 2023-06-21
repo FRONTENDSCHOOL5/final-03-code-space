f5878da7ef0bb82aab9ae1ae27d3da89a1449c32
ChoByungHyun <cbj973@gmail.com>
2023-06-21 20:25:12 +0900

    ♻️ Refactor : 게시글 이미지 캐러셀 효과, 아이콘 변경(#10)

--- src/Components/Feed/Carousel.jsx
+++ src/Components/Feed/Carousel.jsx
@@ -1,27 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const Carousel = ({ imgArr }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [transition, setTransition] = useState(false);
 
+  useEffect(() => {
+    // Enable transition after the initial render
+    setTransition(true);
+  }, []);
   const goToPrevious = () => {
     setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : imgArr.length - 1));
+    setTransition(true);
   };
 
   const goToNext = () => {
     setCurrentIndex(prevIndex => (prevIndex < imgArr.length - 1 ? prevIndex + 1 : 0));
+    setTransition(true);
+  };
+
+  const handleTransitionEnd = () => {
+    setTransition(false);
   };
 
   return (
     <CarouselContainer>
       <CarouselImageContainer>
-        <CarouselImage src={imgArr[currentIndex]} alt="feed" />
+        <CarouselImageWrapper currentIndex={currentIndex}>
+          {imgArr.map((img, index) => (
+            <CarouselImage
+              key={index}
+              src={img}
+              alt="feed"
+              currentIndex={currentIndex}
+              transition={transition}
+              onTransitionEnd={handleTransitionEnd}
+            />
+          ))}
+        </CarouselImageWrapper>
       </CarouselImageContainer>
 
       {imgArr.length > 1 && (
         <>
-          <CarouselButton onClick={goToPrevious}>&lt;</CarouselButton>
-          <CarouselButton onClick={goToNext}>&gt;</CarouselButton>
+          <CarouselButton onClick={goToPrevious} disabled={currentIndex === 0}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </CarouselButton>
+          <CarouselButton onClick={goToNext} disabled={currentIndex === imgArr.length - 1}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </CarouselButton>
         </>
       )}
 
@@ -41,14 +69,22 @@ const CarouselContainer = styled.div`
 const CarouselImageContainer = styled.div`
   max-width: 390px;
   height: 200px;
+  overflow: hidden;
+  perspective: 1000px; /* 3D 효과를 위한 perspective 설정 */
 `;
 
+const CarouselImageWrapper = styled.div`
+  display: flex;
+  transition: transform 0.5s ease; /* 슬라이드 애니메이션 설정 */
+  transform-style: preserve-3d; /* 3D 변환 유지 */
+  transform: translate3d(${({ currentIndex }) => -currentIndex * 100}%, 0, 0); /* X축으로 이동 */
+`;
 const CarouselImage = styled.img`
-  height: 100%;
+  flex-shrink: 0;
   width: 100%;
-  border-radius: 20px;
+  height: 100%;
+  object-fit: cover;
 `;
-
 const CarouselButton = styled.button`
   position: absolute;
   top: 50%;
@@ -57,11 +93,15 @@ const CarouselButton = styled.button`
   border: none;
   font-size: 32px;
   font-weight: bold;
-  color: var(--point-color);
-  cursor: pointer;
+  transition: all 1s;
+  z-index: 1;
+  color: ${({ disabled }) => (disabled ? 'var(--darkgray)' : 'var(--point-color)')};
+  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  opacity: ${({ disabled }) => (disabled ? 0.2 : 1)};
+
   z-index: 1;
 
-  &:hover {
+  &:not(:disabled):hover {
     scale: 1.1;
   }
 
