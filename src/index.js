93e68253465ea65250880125f8e62f1f65112d41
ChoByungHyun <cbj973@gmail.com>
2023-06-26 17:30:03 +0900

    🚀 Deploy : 배포를 위한 코드, 패키지 추가

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { RecoilRoot } from 'recoil';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <RecoilRoot>
       <App />
     </RecoilRoot>
